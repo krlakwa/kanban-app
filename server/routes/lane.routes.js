b1e539e4ee5dec380953e9d56d4190d5757b5361
Krzysztof Łakwa <fenrir14@gmail.com>
2018-07-18 23:22:49 +0200

    Create router for adding new lane

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -3,5 +3,9 @@ import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
 
+// Add a new Lane
+
+router.route('/lanes').post(LaneController.addLane);
+
 
 export default router;
