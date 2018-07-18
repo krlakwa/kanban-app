0f239721aa36e0af2af6a51970a51f0cdb0c1b8d
Krzysztof Łakwa <fenrir14@gmail.com>
2018-07-18 23:18:02 +0200

    Add lane controller

--- server/controllers/lane.controller.js
+++ server/controllers/lane.controller.js
@@ -1,5 +1,20 @@
 import Lane from '../models/lane';
+import uuid from 'uuid';
 
-export function getSomething(req, res) {
-  return res.status(200).end();
+export function addLane(req, res) {
+  if (!req.body.name) {
+    res.status(403).end();
+  }
+
+  const newLane = new Lane(req.body);
+
+  newLane.note = [];
+
+  newLane.id = uuid();
+  newLane.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json(saved);
+  });
 }
