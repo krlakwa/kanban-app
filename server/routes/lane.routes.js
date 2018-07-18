f1ca8b87cbfe027a2db2e5caa22764156cdf5dd3
Krzysztof Łakwa <fenrir14@gmail.com>
2018-07-18 23:29:01 +0200

    Update lane.routes.js

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -3,6 +3,10 @@ import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
 
+// Get all lines
+
+router.route('/lanes').get(LaneController.getLanes);
+
 // Add a new Lane
 
 router.route('/lanes').post(LaneController.addLane);
