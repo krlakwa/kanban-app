c17560cd78e85d7158d626d4935065dfecc101b4
Krzysztof Łakwa <fenrir14@gmail.com>
2018-07-19 00:54:08 +0200

    Add server functionality of adding notes

--- server/routes/note.routes.js
+++ server/routes/note.routes.js
@@ -3,5 +3,8 @@ import * as NoteController from '../controllers/note.controller';
 
 const router = new Router();
 
+// Add a new note
+
+router.route('/notes').post(NoteController.addNote);
 
 export default router;
