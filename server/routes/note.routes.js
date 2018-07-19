0db1b7c5ef142cfef43590fde1613bde54c04bac
Krzysztof Łakwa <fenrir14@gmail.com>
2018-07-19 11:52:25 +0200

    Add edit note content functionality

--- server/routes/note.routes.js
+++ server/routes/note.routes.js
@@ -11,4 +11,8 @@ router.route('/notes').post(NoteController.addNote);
 
 router.route('/notes/:noteId').delete(NoteController.deleteNote);
 
+// Edit note task
+
+router.route('/notes/:noteId').put(NoteController.editNote);
+
 export default router;
