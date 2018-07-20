b23b3b112517c9eab2bfb764444dc844e2fe2e61
Krzysztof Łakwa <fenrir14@gmail.com>
2018-07-20 20:20:46 +0200

    Add edit note to Note actions and reducer

--- client/modules/Note/NoteReducer.js
+++ client/modules/Note/NoteReducer.js
@@ -1,5 +1,5 @@
 // Import Actions
-import { CREATE_NOTE, UPDATE_NOTE, DELETE_NOTE } from './NoteActions';
+import { CREATE_NOTE, UPDATE_NOTE, DELETE_NOTE, EDIT_NOTE } from './NoteActions';
 
 // Initial State
 const initialState = [];
@@ -8,6 +8,10 @@ export default function notes(state = initialState, action) {
   switch (action.type) {
     case CREATE_NOTE:
       return [...state, action.note];
+    case EDIT_NOTE: {
+      const note = { ...state[action.id], editing: true };
+      return { ...state, [action.id]: note };
+    }
     case UPDATE_NOTE:
       return state.map((note) => {
         return note.id === action.id ? { ...note, ...action.note } : note;
