35af1a202840a3f85954f2f98ed74ddb3cf4216b
Krzysztof Łakwa <fenrir14@gmail.com>
2018-07-20 20:05:31 +0200

    Add edit lane to lane actions and reducer

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,5 +1,5 @@
 // Import Actions
-import { CREATE_LANE, UPDATE_LANE, DELETE_LANE } from './LaneActions';
+import { CREATE_LANE, UPDATE_LANE, DELETE_LANE, EDIT_LANE } from './LaneActions';
 import { CREATE_NOTE, DELETE_NOTE } from '../Note/NoteActions';
 
 // Initial State
@@ -9,6 +9,10 @@ export default function lanes(state = initialState, action) {
   switch (action.type) {
     case CREATE_LANE:
       return [...state, action.lane];
+    case EDIT_LANE: {
+      const lane = { ...state[action.id], editing: true };
+      return { ...state, [action.id]: lane };
+    }
     case UPDATE_LANE:
       return state.map(lane => {
         return lane.id === action.id ? { ...lane, ...action.lane } : lane;
