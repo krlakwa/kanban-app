d9aae78ac7993e1b2e02980b620a0395b7ac9aad
Krzysztof Łakwa <fenrir14@gmail.com>
2018-07-24 22:53:47 +0200

    Add styles

--- client/modules/Lane/Lane.js
+++ client/modules/Lane/Lane.js
@@ -12,19 +12,18 @@ const Lane = (props) => {
 
   return (
     <div className={styles.Lane}>
-      <div className={styles.LaneHeader}>
-        <div className={styles.LaneAddNote}>
+      <div>
+        <div>
           <button onClick={() => addNote({ task: 'New Note' }, laneId)}>Add Note</button>
         </div>
         <Edit
-          className={styles.LaneName}
           editing={lane.editing}
           value={lane.name}
           onValueClick={() => editLane(lane.id)}
 
           onUpdate={name => updateLane({ ...lane, name, editing: false })}
         />
-        <div className={styles.LaneDelete}>
+        <div>
           <button onClick={() => deleteLane(laneId)}>Remove Lane</button>
         </div>
       </div>
